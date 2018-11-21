3768df2793cbe7f4dba86dad7fbfc930b87f456a
guidao-me <byte08@126.com>
2018-11-21 13:57:47 +0800

    Go语言数组实现

---
+++ Array/array/array_test.go
@@ -0,0 +1,67 @@
+package array
+
+import (
+	"fmt"
+	"github.com/youtube/vitess/go/vt/log"
+	"testing"
+)
+
+func TestArray_Insert(t *testing.T) {
+	cap := 10
+	arr := NewArray(uint(cap))
+
+
+	for i := 0; i < cap;i++{
+		err := arr.Insert(uint(i),i+100)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	arr.Print()
+}
+
+func TestArray_Delete(t *testing.T) {
+	cap := 10
+	arry := NewArray(uint(cap))
+
+	for i := 0; i < cap; i++{
+		err := arry.Insert(uint(i),i+100)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+
+	arry.Print()
+
+	fmt.Println()
+
+	arry.Delete(2)
+	arry.Delete(5)
+
+	arry.Print()
+}
+
+func TestArray_Find(t *testing.T) {
+	cap := 10
+	arry := NewArray(uint(cap))
+
+	for i := 0; i < cap; i++{
+		err := arry.Insert(uint(i),i+100)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	arry.Print()
+
+	fmt.Println()
+
+	value, err := arry.Find(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("array[%d]=%v\n",1,value)
+}
